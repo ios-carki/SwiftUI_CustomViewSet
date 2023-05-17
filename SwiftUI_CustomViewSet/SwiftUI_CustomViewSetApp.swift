57622b39baf651d6ff21a56234bb299beeb4f0fe
carki <94.nogari@gmail.com>
2023-05-17 23:24:42 +0900

    Initial Commit

---
+++ SwiftUI_CustomViewSet/SwiftUI_CustomViewSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomViewSetApp.swift
+//  SwiftUI_CustomViewSet
+//
+//  Created by Carki on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomViewSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
